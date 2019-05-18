183c4fa1f7bcaf561e40d58d2537d30fea8dfc71
Alireza <alireza.mh@gmail.com>
2019-05-18 23:06:34 +0430

    Add allowSyntheticDefaultImports to true

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 import {Router, Route, Switch} from "react-router";
 import {createBrowserHistory} from "history";
 import {Provider} from "react-redux";
