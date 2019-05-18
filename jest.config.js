6fe3784f15a12f0f40c8f3832987b64465225bbe
Alireza <alireza.mh@gmail.com>
2019-05-18 16:03:48 +0430

    Update react-i18next - ts-jest - jest - es and jest config

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,24 @@
 /*eslint-env node*/
 module.exports = {
-    "moduleFileExtensions": [
-        "ts",
-        "tsx",
-        "js"
-    ],
-        "moduleNameMapper": {
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
-            "\\.(css|less|scss|sass|po)$": "identity-obj-proxy"
-    },
-    "transform": {
-        "^.+\\.(ts|tsx)$": "ts-jest",
-            "^.+\\.js$": "babel-jest"
-    },
-    "globals": {
-        "ts-jest": {
-            "tsConfigFile": "tsconfig.json"
-        }
-    },
-    "testMatch": [
-        "**/__tests__/*.+(ts|tsx|js)"
-    ]
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js"
+  ],
+  "moduleNameMapper": {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less|scss|sass|po)$": "identity-obj-proxy"
+  },
+  "transform": {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.js$": "babel-jest"
+  },
+  "globals": {
+    "ts-jest": {
+      "tsConfigFile": "tsconfig.json"
+    }
+  },
+  "testMatch": [
+    "**/__tests__/*.+(ts|tsx|js)"
+  ]
 }
