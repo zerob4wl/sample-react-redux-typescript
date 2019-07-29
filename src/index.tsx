024540a80613e892ab7f6cd899a9f947b81691fc
alireza-mh <Alireza.mh@gmail.com>
2019-07-29 19:18:35 +0430

    chore: react-router to react-router-dom (#57)

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {Router, Route, Switch} from "react-router";
-import {createBrowserHistory} from "history";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import {Provider} from "react-redux";
 import AppContainer from "./containers/app";
 import {store} from "./redux/store";
-import { I18nextProvider } from "react-i18next";
+import {I18nextProvider} from "react-i18next";
 import i18n from "./translation/i18n";
 
-const history = createBrowserHistory();
 
 ReactDOM.render(
     <Provider store={store}>
         <I18nextProvider i18n={i18n}>
-        <Router history={history}>
-            <Switch>
-                <Route path="/" component={AppContainer}/>
-            </Switch>
-        </Router>
+            <Router>
+                <AppContainer/>
+            </Router>
         </I18nextProvider>
     </Provider>,
     document.getElementById("root")
