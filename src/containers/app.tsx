662125652a154e6951d77d03faf96104d5cf869e
Alireza <alireza.mh@gmail.com>
2019-07-29 13:26:46 +0430

    chore: change react-router to react-router-dom

--- src/containers/app.tsx
+++ src/containers/app.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { hot } from "react-hot-loader/root";
-import {Redirect, Route, Switch} from "react-router";
+import {Redirect, Route, Switch} from "react-router-dom";
 
 import SearchContainer from "./search";
 
