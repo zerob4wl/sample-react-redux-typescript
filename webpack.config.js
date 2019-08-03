3a89484cdb37c56860d7e8380f74b8335ce269f4
Alireza <alireza.mh@gmail.com>
2019-08-03 20:34:28 +0430

    fix: browserlist warning

--- webpack.config.js
+++ webpack.config.js
@@ -36,12 +36,6 @@ const postcssConfig = {
         plugins: () => [
             require("postcss-flexbugs-fixes"),
             autoprefixer({
-                browsers: [
-                    ">1%",
-                    "last 4 versions",
-                    "Firefox ESR",
-                    "not ie < 9", // React doesn"t support IE8 anyway
-                ],
                 flexbox: "no-2009",
             }),
         ],
