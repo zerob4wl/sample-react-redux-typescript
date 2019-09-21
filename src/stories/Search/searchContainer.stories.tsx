086a100a3ee700c44e54274ed5b6ab6190bf0926
Alireza <alireza.mh@gmail.com>
2019-09-21 21:19:23 +0430

    chore: set center addon in config in storybook

--- src/stories/Search/searchContainer.stories.tsx
+++ src/stories/Search/searchContainer.stories.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import { storiesOf } from "@storybook/react";
-import centered from "@storybook/addon-centered/react";
 import {store} from "../../redux/store";
 import {Provider} from "react-redux";
 
@@ -9,7 +8,6 @@ import SearchContainer from "../../containers/search";
 
 
 storiesOf("Search", module)
-  .addDecorator(centered)
   .add("Sample", () => (
       <Provider store={store}>
       <SearchContainer/>
