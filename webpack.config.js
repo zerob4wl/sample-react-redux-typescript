67877bccf93575e2f6bfd3edab3e36240c07c07f
Alireza <alireza.mh@gmail.com>
2019-11-30 22:57:34 +0330

    chore: remove comments in JS

--- webpack.config.js
+++ webpack.config.js
@@ -155,7 +155,14 @@ module.exports = {
   },
   optimization: {
     minimizer: [
-      new TerserJSPlugin({}),
+      new TerserJSPlugin({
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
       new OptimizeCSSAssetsPlugin({
         cssProcessor: require("cssnano"),
         cssProcessorPluginOptions: {
