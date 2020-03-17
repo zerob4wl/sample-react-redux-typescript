b22a15cbc281d582582db35f8bc3845f76e03e9d
Alireza <alireza.mh@gmail.com>
2020-03-17 22:21:30 +0330

    modify: Add type to search component

--- src/containers/search/index.tsx
+++ src/containers/search/index.tsx
@@ -1,57 +1,54 @@
 import React from "react";
-import {useState} from "react";
-import {debounce} from "lodash";
+import { useState } from "react";
+import { debounce } from "lodash";
 import { useTranslation } from "react-i18next";
 import CircleLoader from "react-spinners/CircleLoader";
 import API from "../../lib/api";
 import CityBox from "../../components/CityBox";
 import "./style.less";
 
-const SearchContainer = () => {
-    const [t] = useTranslation();
+interface ISearchContainerProps {}
+const SearchContainer: React.FC<ISearchContainerProps> = () => {
+  const [t] = useTranslation();
 
-    const [touch, setTouch] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const [results, setResult] = useState([]);
+  const [touch, setTouch] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [results, setResult] = useState<any[]>([]);
 
-    const searchForLocation = (e: any) => {
-        if (e.target.value) {
-            fetchData(e.target.value);
-        }
-    };
+  const searchForLocation = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value) {
+      fetchData(e.target.value);
+    }
+  };
 
-    const fetchData = debounce((value: string) => {
-        setTouch(true);
-        setLoading(true);
-        setResult([]);
-        const api = new API();
-        api.searchLocation({
-            query: value,
-        }).then((results) => {
-            setLoading(false);
-            setResult(results);
-        });
-    }, 1000);
-    return (
-        <div className={"container middle center column"}>
-            <h1 className={"search-title"}>{t("Search")}</h1>
-            <input type="text" placeholder={"Berlin"} className={"search-input"}
-                   onChange={searchForLocation}/>
-            <div className={"search-result container middle center space-between"}>
-                <div className="spinner">
-                    <CircleLoader
-                        color={"#ff6369"}
-                        loading={loading}
-                    />
-                    {touch && !loading && results.length === 0 &&
-                    <h5>{t("There is no result!")}</h5>
-                    }
-                </div>
-                {results.map(city => (
-                    <CityBox key={city.woeid} city={city}/>
-                ))}
-            </div>
+  const fetchData = debounce((value: string) => {
+    setTouch(true);
+    setLoading(true);
+    setResult([]);
+    const api = new API();
+    api
+      .searchLocation({
+        query: value,
+      })
+      .then(results => {
+        setLoading(false);
+        setResult(results);
+      });
+  }, 1000);
+  return (
+    <div className={"container middle center column"}>
+      <h1 className={"search-title"}>{t("Search")}</h1>
+      <input type="text" placeholder={"Berlin"} className={"search-input"} onChange={searchForLocation} />
+      <div className={"search-result container middle center space-between"}>
+        <div className="spinner">
+          <CircleLoader color={"#ff6369"} loading={loading} />
+          {touch && !loading && results.length === 0 && <h5>{t("There is no result!")}</h5>}
         </div>
-    );
+        {results.map(city => (
+          <CityBox key={city.woeid} city={city} />
+        ))}
+      </div>
+    </div>
+  );
 };
 export default SearchContainer;
