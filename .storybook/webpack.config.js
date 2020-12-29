4288f9256dbe0799d71a653df952b05d252f0306
Alireza <alireza.mh@gmail.com>
2020-12-29 22:22:15 +0330

    update: December 2020 package update

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,58 +1,60 @@
-const path = require('path');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+const path = require("path");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const HardSourceWebpackPlugin = require("hard-source-webpack-plugin");
 
 module.exports = ({ config }) => {
   config.module.rules.push(
-      {
-        test: /\.(tsx?)/,
-        use: [
-          'babel-loader',
-          {
-            loader: 'react-docgen-typescript-loader',
+    {
+      test: /\.(tsx?)/,
+      use: [
+        "babel-loader",
+        {
+          loader: "react-docgen-typescript-loader",
+        },
+      ],
+    },
+    {
+      test: /\.module.less$/,
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            modules: true,
+            localIdentName: "[name]__[local]___[hash:base64:5]",
           },
-        ],
-      },
-      {
-        test: /\.module.less$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              localIdentName: '[name]__[local]___[hash:base64:5]',
-            },
+        },
+        {
+          loader: "less-loader",
+          options: {
+            sourceMap: true,
           },
-          {
-            loader: 'less-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(less)$/,
-        exclude: /\.module.less$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'less-loader',
-          },
-        ],
-      }
+        },
+      ],
+    },
+    {
+      test: /\.(less)$/,
+      exclude: /\.module.less$/,
+      use: [
+        "style-loader",
+        "css-loader",
+        {
+          loader: "less-loader",
+        },
+      ],
+    }
   );
 
-  config.resolve.extensions.push('.ts', '.tsx');
+  config.resolve.extensions.push(".ts", ".tsx");
   config.plugins = config.plugins.concat([
     new ForkTsCheckerWebpackPlugin({
-      tsconfig: path.resolve(__dirname, '../', 'tsconfig.json'),
+      typescript: {
+        configFile: path.resolve(__dirname, "../", "tsconfig.json"),
+      },
     }),
     new HardSourceWebpackPlugin(),
   ]);
-  config.resolve.alias = { ...config.resolve.alias, src: path.resolve(__dirname, '../', 'src') };
+  config.resolve.alias = { ...config.resolve.alias, src: path.resolve(__dirname, "../", "src") };
 
   return config;
 };
